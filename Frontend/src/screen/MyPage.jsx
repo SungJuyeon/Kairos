c1239d51af87a23c9bcbbbc4dc865c6fc66ba431
3DUCK <kermit7520k@gmail.com>
2024-07-18 13:01:03 +0900

    front

---
+++ Frontend/src/screen/MyPage.jsx
@@ -0,0 +1,49 @@
+import React, { useContext }from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import styled from 'styled-components/native'
+import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from './AuthContext';
+
+export default function MyPage() {
+    const { navigate } = useNavigation();
+
+    // 로그인 관련
+    const { logout } = useContext(AuthContext);
+
+
+    return (
+        <Container>
+          <Title>안녕하세요!</Title>
+            <Button onPress={() => logout()}>
+              <ButtonText>로그 아웃</ButtonText>
+            </Button>
+        </Container>
+    );
+}
+
+const Title = styled.Text`
+    color: white;
+    font-size: 50px;
+    margin-bottom: 40px;
+    font-weight: bold;
+`;
+
+const Container = styled.SafeAreaView`
+    background-color: #000000;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: #FFFFFF;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin: 5px;
+`;
+
+const ButtonText = styled.Text`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+`;
---
+++ Frontend/src/screen/MyPageNavigator.jsx
@@ -0,0 +1,25 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MyPageScreen from "./MyPage";
+import LoginScreen from "./Login";
+import SignInScreen from "./SignIn";
+import FindUserDataScreen from "./FindUserData";
+import FindIdScreen from "./FindId";
+import FindPasswordScreen from "./FindPassword";
+import React from "react";
+
+const MyPageStack = createNativeStackNavigator();
+
+export default function MyPageStackNavigator() {
+  return (
+    <MyPageStack.Navigator screenOptions={{ headerShown: false }}>
+        <MyPageStack.Screen name="Login" component={LoginScreen} />
+        <MyPageStack.Screen name="SignIn" component={SignInScreen} />
+        <MyPageStack.Screen name="MyPage" component={MyPageScreen} />
+        <MyPageStack.Screen name="FindUserData" component={FindUserDataScreen} />
+        <MyPageStack.Screen name="FindId" component={FindIdScreen} />
+        <MyPageStack.Screen name="FindPassword" component={FindPasswordScreen} />
+    </MyPageStack.Navigator>
+  );
+}
