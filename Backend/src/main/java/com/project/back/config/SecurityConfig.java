3141c3197708480620f49d9742cdd03b1643fbe4
Hajin99 <hajin6529@naver.com>
2024-07-18 15:51:05 +0900

    ã밥먹으러

--- Backend/src/main/java/com/project/back/config/SecurityConfig.java
+++ Backend/src/main/java/com/project/back/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:8081");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
