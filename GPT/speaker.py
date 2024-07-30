6840fa48754577264804bb10d85970c3fbb1cbc2
eekfkt <168187962+eekfkt@users.noreply.github.com>
2024-07-30 14:31:05 +0900

    f

--- GPT/speaker.py
+++ GPT/speaker.py
@@ -14,9 +14,8 @@ def speak():
     try:
         text = recognizer.recognize_google(audio, language='ko')
         print("[User] " + text)
+        return text
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print(f"Could not request results from Google Speech Recognition service; {e}")
-
-speak()
