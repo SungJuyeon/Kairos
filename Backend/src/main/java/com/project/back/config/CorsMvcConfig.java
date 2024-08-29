83b141d711d9a469b1f96b8796881c55ad78be63
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2024-08-29 20:00:44 +0900

    join

--- Backend/src/main/java/com/project/back/config/CorsMvcConfig.java
+++ Backend/src/main/java/com/project/back/config/CorsMvcConfig.java
@@ -11,6 +11,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:8083");
+                .allowedOrigins("http://localhost:8081");
     }
 }
+
--- Backend/src/main/java/com/project/back/config/SecurityConfig.java
+++ Backend/src/main/java/com/project/back/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.project.back.config;
 
-
 import java.util.Collections;
 
 import com.project.back.jwt.CustomLogoutFilter;
@@ -26,7 +25,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
-@EnableWebSecurity  //Configuration은 Security를 위한 configure이기 때문에 필요
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -34,63 +33,46 @@ public class SecurityConfig {
     private final JWTUtil jwtUtil;
     private final RefreshRepository refreshRepository;
 
-
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {  //암호화
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
                         CorsConfiguration configuration = new CorsConfiguration();
                         configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
                         configuration.setMaxAge(3600L);
-
                         configuration.setExposedHeaders(Collections.singletonList("Authorization"));
                         return configuration;
                     }
-                }));
-
-        http
-                .csrf((auth) -> auth.disable())     // JWT는 Session을 Stateless 방식으로 하기 때문에 csrf 구현이 필수 X
-
-                .formLogin((auth)->auth.disable())      //http formLogin 방식 disable
-                .httpBasic((auth) -> auth.disable());   //http basic 인증 방식 disable
-
-        http
-                .authorizeHttpRequests((auth) -> auth   //경로별 인가 작업
-                        .requestMatchers("/login", "/", "/join", "/getImage/{id}", "/find/username", "find/password").permitAll()
+                }))
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/", "/join", "/getImage/{id}", "/find/username", "/find/password").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/reissue").permitAll()        //모든 사용자는 access token 만료된 상태로 접근하기에 permitAll
-                        .anyRequest().authenticated() //이외의 요청에는 인증이 필요
-                );
-        http.addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
-
-        // 세션 설정하는 부분 : JWT는 무상태(STATELESS)로 설정
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http
-                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
-        http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil,refreshRepository), UsernamePasswordAuthenticationFilter.class);
+                        .requestMatchers("/reissue").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-
     }
 }
