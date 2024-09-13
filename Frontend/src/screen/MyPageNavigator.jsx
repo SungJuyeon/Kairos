73ee466d29b7f76b61f56a440d43a456b0fa77de
3DUCK <kermit7520k@gmail.com>
2024-09-13 18:52:57 +0900

    복구

--- Frontend/src/screen/MyPageNavigator.jsx
+++ Frontend/src/screen/MyPageNavigator.jsx
@@ -1,10 +1,13 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MyPageScreen from "./MyPage";
 import LoginScreen from "./Login";
-import SignUpScreen from "./SignUp";
+import SignInScreen from "./SignIn";
 import FindUserDataScreen from "./FindUserData";
 import FindIdScreen from "./FindId";
 import FindPasswordScreen from "./FindPassword";
+import ScheduleManageScreen from "./ScheduleManage";
+import FamilyManageScreen from "./FamilyManage";
+import FamilyAddScreen from "./FamilyAdd";
 import React from "react";
 
 const MyPageStack = createNativeStackNavigator();
@@ -19,11 +22,14 @@ export default function MyPageNavigator() {
       headerTransparent: true, // 헤더를 투명하게 설정
     }}>
         <MyPageStack.Screen name="Login" component={LoginScreen} options={{ title: '' }} />
-        <MyPageStack.Screen name="SignUp" component={SignUpScreen} options={{ title: '' }} />
+        <MyPageStack.Screen name="SignIn" component={SignInScreen} options={{ title: '' }} />
         <MyPageStack.Screen name="MyPage" component={MyPageScreen} options={{ title: '' }} />
         <MyPageStack.Screen name="FindUserData" component={FindUserDataScreen} options={{ title: '' }} />
         <MyPageStack.Screen name="FindId" component={FindIdScreen} options={{ title: '' }} />
         <MyPageStack.Screen name="FindPassword" component={FindPasswordScreen} options={{ title: '' }} />
+        <MyPageStack.Screen name="ScheduleManage" component={ScheduleManageScreen} options={{ title: '' }} />
+        <MyPageStack.Screen name="FamilyManage" component={FamilyManageScreen} options={{ title: '' }} />
+        <MyPageStack.Screen name="FamilyAdd" component={FamilyAddScreen} options={{ title: '' }} />
     </MyPageStack.Navigator>
   );
 }
